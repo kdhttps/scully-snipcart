d0deb0b77079beb739f16407c961b47a1e6acd30
kdhttps <kdhttps@gmail.com>
2020-06-10 18:32:25 +0530

    first commit

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user-routing.module';
+import { UserComponent } from './user.component';
+import { UsersComponent } from './users.component';
+
+
+@NgModule({
+  declarations: [
+    UserComponent,
+    UsersComponent,
+  ],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+  ]
+})
+export class UserModule { }
